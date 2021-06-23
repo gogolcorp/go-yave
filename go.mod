e9155ad5036c012fa6560828844a471454cd45dd
blyndusk <alexandre.delaloy.pro@gmail.com>
2021-06-23 12:21:42 +0200

    delete(files): #18 - old files

--- go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/blyndusk/repo-template
-
-go 1.13
-
-require github.com/spf13/cobra v1.1.3
--- main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/blyndusk/repo-template/cmd"
-
-func main() {
-  cmd.Execute()
-}

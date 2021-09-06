d0dcf23f6ee89c02a7ed540cca2db1e9e6ac912f
blyndusk <alexandre.delaloy.pro@gmail.com>
2021-09-06 14:30:36 +0200

    update(fs): #22 - build folder for docker

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-cp deployments/docker-compose.yml docker-compose.yml
+# set current docker-compose file
+cp deploy/docker-compose.yml docker-compose.yml
 
+# if actual .env exist, ask for saving it
 if [ -f .env ]; then
-echo "oui"
-  read -p "Replace .env file ? [Y/n]: " -n 1 -r YN
+  read -p "Save old .env file ? [Y/n]: " -n 1 -r YN
   if [[ $YN =~ ^[Yy]$ || $YN = "" ]] ; then
     mv .env .env.old
   fi
 fi
+
+# replace it anyway
 cp .env.sample .env
